328e736cc57a316f983169aa19fbbd8a131cb079
annakhuseinova <annakhuseinova@yandex.ru>
2021-06-13 23:16:06 +0300

    Initial commit

---
+++ src/main/java/com/annakhuseinova/springcloudstreamsxmltojson/SpringCloudStreamsXmlToJsonApplication.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.springcloudstreamsxmltojson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamsXmlToJsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStreamsXmlToJsonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/annakhuseinova/springcloudstreamsxmltojson/SpringCloudStreamsXmlToJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.springcloudstreamsxmltojson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamsXmlToJsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
